8d4aa6585c0b829be30f762fdc5c4ff60922df57
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-30 15:11:03 +0700

    menambahkan validasi list kosong dan inisialisasi input serta pointer untuk proses penghapusan node

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -70,6 +70,17 @@ class DoubleLinkedList
 
     void hapus()
     {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+        }
+
+        cout << "\nEnter the roll number of the student whose record is to be deleted : ";
+        int rollNo;
+        cin >> rollNo;
+
+        Node *current = START;
 
     }
 };
