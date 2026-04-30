6552403c4755e7ab5e4c293ab4b60222056db64f
fazila7002 <fazilatunnisa705@gmail.com>
2026-04-30 16:06:09 +0700

    implementasi menu interaktif dengan pemilihan operasi menggunakan perulangan dan percabangan switch untuk memanggil fungsi pada object DoubleLinkedList

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -159,7 +159,88 @@ class DoubleLinkedList
         }
     }
 
+    void serachData()
+    {
+        if (START == NULL)
+        {
+            cout << "\nList is empty" << endl;
+            return;
+
+        }
+
+        int rollNo;
+        cout << "\nEnter the roll number to search: ";
+        cin >> rollNo;
+
+        Node *current = START;
+
+        while (current != NULL && current->NoMhs != rollNo)
+        current = current->next;
+
+        if (current == NULL)
+        {
+            cout << "Record not found\n";
+        }
+        else
+        {
+            cout << "Record found\n";
+            cout << "Roll Number: " << current->NoMhs << endl;
+        }
+    }
+
+    int main() 
+    {
+
     
+    DoubleLinkedList list;
+    char choice;
+
+    do
+    {
+        cout << "\nMenu:\n";
+        cout << "1. Add Record\n";
+        cout << "2. Delete Record\n";
+        cout << "3. View Ascending\n";
+        cout << "4. View Descending\n";
+        cout << "5. Search Record\n";
+        cout << "6. Exit\n";
+        cout << "Enter your choice: ";
+
+        cin >> choice;
+
+        switch (choice)
+        {
+        case '1':
+            list.addNode();
+            break;
+        case '2':
+            list.hapus();
+            break;
+        case '3':
+            list.traverse();
+            break;
+        case '4':
+            list.retraverse();
+            break;
+        case '5':
+            list.serachData();
+            break;
+        case '6':
+            return 0;
+        default:
+            cout << "Invalid option\n";
+        }
+
+
+        cout << "\nPress Enter to continue...";
+        cin.ignore();
+        cin.get();
+        cout << endl;
+
+    }while (choice != '6');
+    }
+    
+
 
 
 };
